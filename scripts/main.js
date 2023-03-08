27016e1ceb26c6415ceed93f1e9ae2320a89c401
dream-tentacle <127286614+dream-tentacle@users.noreply.github.com>
2023-03-08 20:35:51 +0800

    添加新内容
    将原来的index改为了newdoor, 新增了主页内容

--- scripts/main.js
+++ scripts/main.js
@@ -8,23 +8,3 @@ myImage.onclick = function () {
         myImage.setAttribute('src', 'images/the_beachcomber.png');
     }
 }
-let myButton = document.querySelector('button');
-let myHeading = document.querySelector('h1');
-function setUserName() {
-    let myName = prompt('报上你的名讳 :');//获取用户输入
-    if (!myName) setUserName();
-    else {
-        localStorage.setItem('name', myName);
-        myHeading.textContent = myName + ', 欢迎来到我们的世界...';
-    }
-}
-
-if (!localStorage.getItem('name')) {
-    setUserName();
-} else {
-    let storedName = localStorage.getItem('name');
-    myHeading.textContent = storedName + ', 欢迎来到我们的世界...';
-}
-myButton.onclick = function () {
-    setUserName();
-}
