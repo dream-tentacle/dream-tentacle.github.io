0ef5b0b43d81eec6594d6f02d7d7e06cab1ab5e6
dream-tentacle <127286614+dream-tentacle@users.noreply.github.com>
2023-03-18 15:30:46 +0800

    优化UI

--- pages/'me'/src/components/Ability.js
+++ pages/'me'/src/components/Ability.js
@@ -4,12 +4,10 @@ function Ability(props) {
   return (
     <div className="Ability">
       <h4>你的属性:</h4>
-      <ul>
-        <li className="Cornflowerblue">洞察(目): {props.observe}</li>
-        <li className="Tomato">抵抗(心): {props.defend}</li>
-        <li className="Lightslategray">技艺(珠): {props.technique}</li>
-        <li className="Blue">欲望(水): {props.desire}</li>
-      </ul>
+      <span className="Cornflowerblue">目: {props.observe}</span>
+      <span className="Tomato">心: {props.defend}</span>
+      <span className="Lightslategray">珠: {props.technique}</span>
+      <span className="Blue">水: {props.desire}</span>
     </div>
   );
 }
